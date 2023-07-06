0431e0052bed9772b30abd32d6a78224cb1fd4af
Jan Gloe <jan.gloe@ernstings-family.com>
2023-07-06 08:08:28 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+app.get("/name", (req, res) => {
+ let { first, last } = req.query;
+ console.log({"name": `${first} ${last}`});
+ res.json({"name": `${first} ${last}`})
+})
+
 
 
 
